369d7f2760ceeabd04192fbaad2df3ab5e53a795
wold21 <wold2180@gmail.com>
2022-07-17 16:33:32 +0900

    - functional components and class componenets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="T" nickName="O">
+        <p>This is children element</p>
+      </Greet>
+      <Greet name="A" nickName="Z">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Z" nickName="A"/>
+      <Greet name="O" nickName="T"/>
+      <Welcome name="T" nickName="O"/>
+      <Welcome name="T" nickName="O"/>
+      <Welcome name="T" nickName="O"/>
     </div>
   );
 }
---
+++ src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Greet = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h1>Hello {props.name} a.k.a {props.nickName}</h1>
+            {props.children}
+        </div>
+    )
+}
+
+export default Greet
