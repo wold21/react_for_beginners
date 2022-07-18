ba0fe72dd0e40e2799bdcfa5668925bbb9afdfd3
wold21 <wold2180@gmail.com>
2022-07-18 22:43:20 +0900

    - setState

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Message/>
+      <Counter/>
+      {/* <Message/> */}
       {/* <Greet name="T" nickName="O">
         <p>This is children element</p>
       </Greet>
