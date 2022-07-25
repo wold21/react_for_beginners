805bababf8e2b5ee87991b248d5c6e296df3cca5
wold21 <wold2180@gmail.com>
2022-07-25 21:16:05 +0900

    - 부모 컴포넌트에서 자식 컴포넌트로 함수 전달 및 자식에서 부모로 데이터 전달

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Counter from "./components/Counter";
 import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
 import EventBind from "./components/EventBind";
+import ParentComponent from "./components/ParentComponent";
 
 function App() {
     return (
@@ -24,8 +25,9 @@ function App() {
             <Welcome name="T" nickName="O" />
             <Message />
             <FunctionClick />
-            <ClassClick /> */}
-            <EventBind />
+            <ClassClick /> 
+            <EventBind />*/}
+            <ParentComponent />
         </div>
     );
 }
