6597f8b02bba7a34ddaa1158c4feb7292bdc106e
wold21 <wold2180@gmail.com>
2022-10-10 19:23:38 +0900

    render props pattern

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import "./App.css";
 import ClickCounter from "./components/ClickCounter";
+import ClickCounterTwo from "./components/ClickCounterTwo";
+import Counter from "./components/Counter";
 import HoverCounter from "./components/HoverCounter";
+import HoverCounterTwo from "./components/HoverCounterTwo";
+import User from "./components/User";
 
 function App() {
     return (
         <div className="App">
-            <ClickCounter name={"Tazo"} />
+            {/* <ClickCounter name={"Tazo"} />
             <HoverCounter />
+            <ClickCounterTwo />
+            <HoverCounterTwo />
+            <User render={(isLoggedIn) => (isLoggedIn ? "Tazo" : "Guest")} />*/}
+            <Counter>
+                {(count, incrementCount) => (
+                    <ClickCounterTwo count={count} incrementCount={incrementCount} />
+                )}
+            </Counter>
+            <Counter>
+                {(count, incrementCount) => (
+                    <HoverCounterTwo count={count} incrementCount={incrementCount} />
+                )}
+            </Counter>
         </div>
     );
 }
